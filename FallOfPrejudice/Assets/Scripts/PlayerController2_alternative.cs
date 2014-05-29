f427ed5f4917ccb8a3cbb8a92d398a9112f8c81b
Sergej Laas <slaas@mail.uni-paderborn.de>
2014-05-30 00:57:43 +0200

    * added ignore file * added some test files

---
+++ FallOfPrejudice/Assets/Scripts/PlayerController2_alternative.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController2 : MonoBehaviour {
+
+	public float speed = 6.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+	private Vector3 moveDirection = Vector3.zero;
+
+	private bool facingRight = true;
+
+	void Update() {
+		CharacterController controller = GetComponent<CharacterController>();
+		float move = Input.GetAxis ("Horizontal2");
+		if (controller.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal2"), 0, 0);
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton("Jump2"))
+				moveDirection.y = jumpSpeed;
+			
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+		
+		if (move > 0 && !facingRight)
+			Flip ();
+		else if (move < 0 && facingRight)
+			Flip ();
+	}
+	
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+}
