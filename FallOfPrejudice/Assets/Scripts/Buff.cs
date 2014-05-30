1b305d5922da2655f0d23e34d7d7f79ba5cf3202
Sergej Laas <slaas@mail.uni-paderborn.de>
2014-05-30 16:55:54 +0200

    * added buff and obstacle prefabs * rewrote the corresponding scripts

--- FallOfPrejudice/Assets/Scripts/Buff.cs
+++ FallOfPrejudice/Assets/Scripts/Buff.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class Buff : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	public float currentSpeed;
+	public float healthGain;
+
 	// Update is called once per frame
 	void Update () {
-	
+		float amtToMove = currentSpeed * Time.deltaTime;
+		transform.Translate (Vector3.down * amtToMove);
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player") {
+			Destroy (gameObject);
+			col.gameObject.GetComponentInChildren<Bubble>().health += healthGain;
+		} else if (col.gameObject.tag == "Ground") {
+			Destroy (gameObject);
+		}
 	}
 }
--- FallOfPrejudice/Assets/Scripts/Obstacle.cs
+++ FallOfPrejudice/Assets/Scripts/Obstacle.cs
@@ -3,16 +3,8 @@ using System.Collections;
 
 public class Obstacle : MonoBehaviour {
 	
-	public float MinSpeed;
-	public float MaxSpeed;
-	
-	private float currentSpeed;
-	private float x, y, z;
-	
-	// Use this for initialization
-	void Start () {
-		SetPositionAndSpeed();
-	}
+	public float currentSpeed;
+	public float healthDrain;
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,12 +12,13 @@ public class Obstacle : MonoBehaviour {
 		transform.Translate (Vector3.down * amtToMove);
 	}
 	
-	public void SetPositionAndSpeed()
+	void OnTriggerEnter2D(Collider2D col)
 	{
-		currentSpeed = Random.Range (MinSpeed, MaxSpeed);
-		x = Random.Range (-7f, 7f);
-		y = 7.0f;
-		z = 0.0f;
-		transform.position = new Vector3 (x, y, z);
+		if (col.gameObject.tag == "Player") {
+			Destroy (gameObject);
+			col.gameObject.GetComponentInChildren<Bubble>().health -= healthDrain;
+		} else if (col.gameObject.tag == "Ground") {
+			Destroy (gameObject);
+		}
 	}
 }
