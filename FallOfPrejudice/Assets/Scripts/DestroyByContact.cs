e0239fcfc5996d4377d609bea20bc7fc32c49db6
Sergej Laas <slaas@mail.uni-paderborn.de>
2014-05-30 15:27:45 +0200

    added daniels work

---
+++ FallOfPrejudice/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour 
+{
+	void Start ()
+	{
+	}
+	
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player") {
+			Destroy (gameObject);
+			col.gameObject.GetComponentInChildren<Bubble>().health -= 20;
+		} else if (col.gameObject.tag == "Ground") {
+			Destroy (gameObject);
+		}
+	}
+}
