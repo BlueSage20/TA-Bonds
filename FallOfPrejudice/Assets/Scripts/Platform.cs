c5423dcdfc590c147ccc55cb78851a63309a82d0
Sergej Laas <slaas@mail.uni-paderborn.de>
2014-05-30 18:05:57 +0200

    added platform

---
+++ FallOfPrejudice/Assets/Scripts/Platform.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : MonoBehaviour {
+
+	public float currentSpeed;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float amtToMove = currentSpeed * Time.deltaTime;
+		transform.Translate (Vector3.down * amtToMove);
+	}
+}
