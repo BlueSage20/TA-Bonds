8474325643d95d9630c4c1d01a4f81dec548f423
Daniel Zart <danielzart94@gmail.com>
2014-05-30 10:33:17 +0200

    Newest version with 2D physics

---
+++ FallOfPrejudice/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+	void Update()
+	{
+		if (transform.position.y <= -3) {
+			Destroy(gameObject);		
+		}
+	}
+}
--- FallOfPrejudice/Assets/Scripts/DestroyByGround.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class DestroyByGround : MonoBehaviour {
-	void OnTriggerExit(Collider other) {
-		Destroy (this.gameObject);
-	}	
-}
