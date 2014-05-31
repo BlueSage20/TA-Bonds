e7203424e0ce957494840828ce510d463e4f5c15
Sergej Laas <slaas@mail.uni-paderborn.de>
2014-05-31 04:58:00 +0200

    reworked buff scripts.. two scripts now: static and moving buff

---
+++ FallOfPrejudice/Assets/Scripts/BuffStatic.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuffStatic : MonoBehaviour {
+
+	public float spawnTime;
+	public float despawnTime;
+	public float healthGain;
+
+	void Start()
+	{
+		transform.renderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Time.realtimeSinceStartup >= spawnTime)
+		{
+			transform.renderer.enabled = true;
+			if(Time.realtimeSinceStartup >= despawnTime)
+			{
+				transform.renderer.enabled = false;
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player") {
+			Destroy (gameObject);
+			col.gameObject.GetComponentInChildren<Bubble>().health += healthGain;
+		} else if (col.gameObject.tag == "Ground") {
+			Destroy (gameObject);
+		}
+	}
+}
